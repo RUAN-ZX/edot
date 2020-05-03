089ead045f649736fcbd1079fdcb39113f62a63b
1162179851@qq.com <3269469626@qq.com>
2020-05-03 16:32:36 +0800

    init blank

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(edotApp());
 
-class edotApp extends StatelessWidget {
-  // This widget is the root of your application.
+main(List<String> args) => edot();
+
+class edot extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return MaterialApp(
-      title: 'edotDemo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primaryColor: Colors.lightBlueAccent,
+      home: Scaffold(
+
       ),
-//      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
+
 }
