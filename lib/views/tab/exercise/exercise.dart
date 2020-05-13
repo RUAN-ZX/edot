4e76e0f99d91d1f0be16e30620b10778813b2849
RUAN-ZX <https://github.com/RUAN-ZX>
2020-05-14 00:38:44 +0800

    musucle Png Added

--- lib/views/tab/exercise/exercise.dart
+++ lib/views/tab/exercise/exercise.dart
@@ -28,8 +28,65 @@ class exercise extends StatelessWidget{
   }
 
 }
+
+
+class musucleBtn extends StatelessWidget {
+  final int index;
+//  final double y;
+//  final double width;
+//  final double height;
+//  final String btnSrcPressed;
+//  final String btnSrcUnPressed;
+  final Function func;
+  final bool frontOrBack;
+  //"asset/images/back/b_back.png"
+  musucleBtn(
+      this.index,
+      this.frontOrBack,
+//      this.y,
+//      this.width,
+//      this.height,
+//      this.btnSrcPressed,
+//      this.btnSrcUnPressed,
+      this.func,
+      );
+  String btnSrc = '';
+  @override
+  Widget build(BuildContext context) {
+
+    if(frontOrBack){btnSrc='asset/images/front/';}
+    else {btnSrc='asset/images/back/';}
+
+    return new Positioned(
+      //true front False=back
+
+      left: backMusucleData[index-1]['x'],
+      top: backMusucleData[index-1]['y'],
+      child: ImageButton(
+        children: <Widget>[],
+        width: backMusucleData[index-1]['width'],
+        height: backMusucleData[index-1]['height'],
+        paddingTop: 5,
+        pressedImage: Image.asset(btnSrc+backMusucleData[index-1]['btnSrcPressed']),
+        unpressedImage: Image.asset(btnSrc+backMusucleData[index-1]['btnSrcUnPressed']),
+        onTap: func,
+      )
+    );
+  }
+}
+
 class exerciseBody extends StatelessWidget{
   final size;
+  Function printR(value){
+    print("${value}肌");
+  }
+//  final List<String> BtnSrc = [
+//    "asset/images/back/b_back.png",
+//    "asset/images/back/b_back.png",
+//    "asset/images/back/b_back.png"
+//    "asset/images/back/b_back.png"
+//
+//  ];
   const exerciseBody(this.size);
   @override
   Widget build(BuildContext context) {
@@ -40,25 +97,8 @@ class exerciseBody extends StatelessWidget{
             Expanded(
               child: new Stack(
                 children: <Widget>[
-//                  new Image.asset("asset/images/back/b.png",width: double.infinity,fit: BoxFit.cover,),
-                  new Positioned(
-                      left:0.0,
-                      right: 0.0,
-                      top: 0.0,
-                      child: ImageButton(
-                        children: <Widget>[],
-                        width: size.width/4,
-                        height: size.height/4,
-                        paddingTop: 5,
-                        pressedImage: Image.asset(
-                          "asset/images/back/b_back.png",
-                        ),
-                        unpressedImage: Image.asset("asset/images/back/b_back2.png"),
-                        onTap: () {
-                          print('test');
-                        },
-                      )
-                      )
+                  musucleBtn(1,false,printR("背")),
+
                   ]
 //                      Image.asset(,width: double.infinity,fit: BoxFit.cover,),,
                 ),
@@ -72,27 +112,17 @@ class exerciseBody extends StatelessWidget{
                   borderSide:new BorderSide(color: Theme.of(context).primaryColor),
                   child: Padding(
                     padding: EdgeInsetsDirectional.fromSTEB(5, 10, 5, 10),
-  //                  child: new Text('注册\n234\n234',style: new TextStyle(color: Theme.of(context).primaryColor),),
                     child: RichText(
                       text: TextSpan(
-  //                      text: '本人同意领取赠险并许可保险公司沟通赠险生效事宜,已阅读',
                         style: TextStyle(color: Colors.black, fontSize: 13),
                         children: <TextSpan>[
                           TextSpan(
                               text: '背阔肌\n',
                               style:TextStyle(color: Color(0xFF008EFF), fontSize: 13,fontWeight: FontWeight.bold),
-  //                            recognizer: TapGestureRecognizer()
-  //                              ..onTap = () async {
-  //                                AgreementDialog.showActivityDialog(context);
-  //                              }
                                 ),
                           TextSpan(
                               text: 'latissimus dorsi位于腰背部和胸部后外侧皮下，为全身最大的阔肌，',
                               style:TextStyle(color: Color(0xFF008EFF), fontSize: 13),
-  //                            recognizer: TapGestureRecognizer()
-  //                              ..onTap = () async {
-  //                                AgreementDialog.showSecurityDialog(context);
-  //                              }
                             ),
                         ],
                       ),
@@ -104,126 +134,5 @@ class exerciseBody extends StatelessWidget{
             ),
         ]
     ),
-    );
-//            new Image.asset("asset/images/back/b.png",width: 140),
-
-
-//        new ListView(
-//          children: <Widget>[
-//            Text("背阔肌"),
-//            Text("背阔肌简介"),
-//          ],
-//        ),
-  }
+  );
 }
-
-
-//  List<Widget> _getListData() {
-//    var tempList=listData.map((value){
-//      return Container(
-//        child:Column(
-//          children: <Widget>[
-//            Image.network(value['imageUrl']),
-//            SizedBox(height: 12),
-//            Text(
-//              value['title'],
-//              textAlign: TextAlign.center,
-//              style: TextStyle(
-//                  fontSize: 20
-//              ),
-//            )
-//          ],
-//
-//        ),
-//        decoration: BoxDecoration(
-//
-//            border: Border.all(
-//                color:Color.fromRGBO(233, 233,233, 0.9),
-//                width: 1
-//            )
-//        ),
-//
-//        // height: 400,  //设置高度没有反应
-//      );
-//
-//    });
-//    // ('xxx','xxx')
-//    return tempList.toList();
-//  }
-
-
-//  @override
-//  Widget build(BuildContext context) {
-//    return GridView.count(
-//      crossAxisSpacing:0.0 ,   //水平子 Widget 之间间距
-//      mainAxisSpacing: 0.0,    //垂直子 Widget 之间间距
-//      padding: EdgeInsets.all(10),
-//      crossAxisCount: 5,  //一行的 Widget 数量
-//      // childAspectRatio:0.7,  //宽度和高度的比例
-//      children: this._getListData(),
-//    );
-//  }
-//}
-
-//class exerciseBody extends StatefulWidget {
-//  @override
-//  _exerciseBodyState createState() => _exerciseBodyState();
-//}
-//
-//class _exerciseBodyState extends State<exerciseBody> {
-//  var result;
-//  @override
-//  void initState() {
-//    super.initState();
-//
-//    FormData formData = FormData.fromMap({
-//      "mId": "40",
-////      "age": 25,
-////      "file": await MultipartFile.fromFile("./text.txt",filename: "upload.txt"),
-////      "files": [
-////        await MultipartFile.fromFile("./text1.txt", filename: "text1.txt"),
-////        await MultipartFile.fromFile("./text2.txt", filename: "text2.txt"),
-////      ]
-//    });
-//    /// 自己封装的 兼容各种类型的请求
-//    /// 注意 post方式要传 FormData格式的postdata
-//    /// get方式 如果有url参数 要传 Map<String,dynamic>的queryParams
-//    HttpRequest.request("getMessage/",method: "post",postdata: formData).then((res){
-//      setState(() {
-//        this.result = res;
-//      });//这里 一旦访问完成立刻幅值 立刻让其他人通过result拿到值！
-//      final a = this.result.data['msUserId'];
-////      print(a);
-//    });
-//
-//    HttpRequest.request("getMessage/",method: "get").then((res){
-//      setState(() {
-//        this.result = res;
-//      });
-//      final a = this.result.data;
-////      print(a);
-//    });
-//
-//
-//  }
-//  @override
-//  Widget build(BuildContext context) {
-//    return GridView(
-//      scrollDirection: Axis.vertical,
-//      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-//        maxCrossAxisExtent: 100, //子控件最大宽度为100
-//        childAspectRatio: 1.0,//宽高比为1:2
-//        crossAxisSpacing: 10,
-//        mainAxisSpacing: 10,
-//      ),
-//      padding: EdgeInsets.all(10),
-//      children: List.generate(
-//        20,
-//            (index) {
-//          return null;
-//        },
-//      ),
-//    );
-//
-//  }
-//}
--- lib/views/tab/exercise/listData.dart
+++ lib/views/tab/exercise/listData.dart
@@ -36,3 +36,80 @@
       }      
 
   ];
+
+List backMusucleData=[
+  //deltoid 三角肌
+  {
+    "x": 1/4,
+    "y": 1/20,
+    "width": 20/36,
+    "height": 6/76,
+    "btnSrcPressed": "b_deltoid2.png",
+    "btnSrcUnPressed": "b_deltoid1.png",
+  },
+
+  //arm 手臂
+  {
+    "x": 1/20,
+    "y": 1/10,
+    "width": 0.9,
+    "height": 200/760,
+    "btnSrcPressed": "b_arm2.png",
+    "btnSrcUnPressed": "b_arm1.png",
+  },
+
+  //背阔肌 back
+  {
+    "x": 1/3.6,
+    "y": 1/10,
+    "width": 16/36,
+    "height": 200/760,
+    "btnSrcPressed": "b_back2.png",
+    "btnSrcUnPressed": "b_back1.png",
+  },
+
+  // 后腰肌 psoas
+  {
+    "x": 1/3.6,
+    "y": 5/19,
+    "width": 11/36,
+    "height": 12/76,
+    "btnSrcPressed": "b_psoas2.png",
+    "btnSrcUnPressed": "b_psoas1.png",
+  },
+
+  // 屁股 ass
+  {
+    "x": 1/4,
+    "y": 1/2,
+    "width": 1/2,
+    "height": 13/76,
+    "btnSrcPressed": "b_ass2.png",
+    "btnSrcUnPressed": "b_ass1.png",
+  },
+
+  // 大腿 thigh
+  {
+    "x": 1/4,
+    "y": 23/38,
+    "width": 1/2,
+    "height": 15/76,
+    "btnSrcPressed": "b_thigh2.png",
+    "btnSrcUnPressed": "b_thigh1.png",
+  },
+
+  // 小腿 leg
+  {
+    "x": 1/6,
+    "y": 15/19,
+    "width": 2/3,
+    "height": 17/76,
+    "btnSrcPressed": "b_leg2.png",
+    "btnSrcUnPressed": "b_leg1.png",
+  },
+
+
+];
+
+
+
